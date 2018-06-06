60008e122f774a724eb2ab1adeb8521cece09dab
jacquesbeets <jacques.beets@gmail.com>
2018-06-06 12:20:47 +0200

    Completed Lists Module

---
+++ 5-Lists/list.py
@@ -0,0 +1,101 @@
+# friends = ["Ashley", "Matt", " Michael"]
+
+# print(friends[1])
+
+# print("Matt" in friends) #Check if value is in list RETURNS BOOLEAN
+
+
+# =====================
+# LOOP THROUGH NUMBERS 
+# =====================
+# numbers = [1,2,3,4,5,6,7,8]
+# friends = ["Ashley", "Matt", "Michael"]
+
+# # for number in numbers:
+# #   print(number * number)
+
+# # for friend in friends:
+# #   print(friend)
+
+# i = 0
+
+# while i < len(numbers):
+#   print(numbers[i])
+#   i += 1
+
+# index = 0
+
+# while index < len(friends):
+#   print(f"{index}: {friends[index]}")
+#   index += 1'
+
+# =====================
+# EXCERSIZE
+# =====================
+
+# sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+# result = ""
+
+# for sound in sounds:
+#   result = result + sound.upper()
+
+# =====================
+# LIST METHODS
+# =====================
+
+data = [1,2,3,4,5, "Jack", "Tom", "Harry", "Tom"]
+# print(data)
+# data.append(6)
+# print(data)
+# data.extend([7,8,9,10])
+# print(data)
+
+# data.insert(2, "Hello")
+# print(data)
+# data.insert(len(data), "The End")
+# print(data)
+
+# data.clear() #clears the whole list
+# print(data)
+
+# data.pop()
+# print(data)
+# data.pop(2)
+# print(data)
+
+# data.remove(2) #finds the value in your list that equal to the value specified and removes it.
+# print(data)
+
+# print(data.index("Tom"))
+
+# print(data.count("Tom")) #counts instancis in a list
+
+# data.reverse()
+
+# print(data)
+
+# data.sort()
+# print(data)
+
+# data = ["Coding", "is", "Fun"]
+# print(" ".join(data))
+
+#SLICING - returns a copy of data list
+# data[start:end:step]
+
+# print(data[2:6:1])
+# print(data[3:]) #slice to index 3
+# print(data[:3]) #slice from index 3
+# print(data[1::2])
+
+# string = "This is fun"
+# print(string[::-1])
+
+# =====================
+# EXCERSIZE
+# =====================
+
+# numbers = [1,2,3,4]
+# # print(numbers[1:3])
+# print(numbers[-2])
+
